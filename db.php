5e52cafd4c6a15d83551db02b7ac5a33a1fef0ad
mlerin558521-afk <m.lerin.558521@umindanao.edu.ph>
2026-03-03 13:04:24 +0800

    Update db.php

--- db.php
+++ db.php
@@ -10,7 +10,7 @@ try {
         $username,
         $password
     );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ✅ correct variable
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
